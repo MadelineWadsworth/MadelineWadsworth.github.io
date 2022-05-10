bc3068f001e36c57107aa59beb49741c585d561c
MaddieJane <67866550+MadelineJaneWadsworth@users.noreply.github.com>
2022-05-10 08:46:26 -0400

    added code

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+  background(0);
+  createCanvas(500, 500);
+}
+
+function draw () {
+  fill('pink');
+  ellipse(100, 100, 100);
+  fill('aqua');
+  ellipse(250,100,100);
+  fill('purple');
+  ellipse(400,100, 100);
+}
