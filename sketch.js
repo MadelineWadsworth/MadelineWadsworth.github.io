aa6e21aeb197170be18d211fa7b3d45422787987
MaddieJane <67866550+MadelineJaneWadsworth@users.noreply.github.com>
2022-05-11 09:52:53 -0400

    added background and began to add text (name)

--- sketch.js
+++ sketch.js
@@ -1,13 +1,55 @@
 function setup() {
   background(0);
-  createCanvas(500, 500);
+  createCanvas(windowWidth, windowHeight);
+  push();
+  var size = 100;
+
+   for (x = 0; x < windowWidth; x = x + 70) {
+     for (y = 0; y < windowHeight; y = y + 70) {
+
+        fill(255, 105, 180, 200);
+        heart(x, y, 60, 60);
+        fill(248, 200, 220, 200);
+        heart(x,y,50, 40);
+        fill(255, 182, 193, 200);
+        heart(x, y, 40, 30);
+        size = size - 0.5;}
+
+    fill(0);
+    textSize(50);
+    textAlign(CENTER);
+    text('MADELINE', windowWidth/2, windowHeight/2);
+    pop();
+}
 }
 
 function draw () {
-  fill('pink');
-  ellipse(100, 100, 100);
-  fill('aqua');
-  ellipse(250,100,100);
-  fill('purple');
-  ellipse(400,100, 100);
+  // push();
+  // var size = 100;
+  //
+  //  for (x = 0; x < windowWidth; x = x + 70) {
+  //    for (y = 0; y < windowHeight; y = y + 70) {
+  //
+  //       fill(255, 105, 180, 63);
+  //       heart(x, y, 60, 60);
+  //       fill(248, 200, 220, 63);
+  //       heart(x,y,50, 40);
+  //       fill(255, 182, 193, 63);
+  //       heart(x, y, 40, 30);
+  //       size = size - 0.5;}
+  //
+  //   fill(0);
+  //   textSize(50);
+  //   textAlign(CENTER);
+  //   text('MADELINE', windowWidth/2, windowHeight/2);
+  //   pop();
+   
 }
+
+function heart(x, y, size) {
+    beginShape();
+    vertex(x, y);
+    bezierVertex(x - size / 2, y - size / 2, x - size, y + size / 3, x,     y + size);
+    bezierVertex(x + size, y + size / 3, x + size / 2, y - size / 2, x,     y);
+    endShape(CLOSE);
+  }
