a2bdf8de972e510a7a601048ec0e702c98097895
MaddieJane <67866550+MadelineJaneWadsworth@users.noreply.github.com>
2022-05-21 14:56:59 -0400

    added links to old projects

--- sketch.js
+++ sketch.js
@@ -1,3 +1,11 @@
+var a;
+
+var m;
+
+var k;
+
+var l;
+
 function setup() {
   background(0);
   createCanvas(windowWidth, windowHeight);
@@ -14,37 +22,40 @@ function setup() {
         fill(255, 182, 193, 200);
         heart(x, y, 40, 30);
         size = size - 0.5;}
-
-    fill(0);
-    textSize(50);
-    textAlign(CENTER);
-    text('MADELINE', windowWidth/2, windowHeight/2);
-    pop();
+pop();
 }
 }
 
 function draw () {
-  // push();
-  // var size = 100;
-  //
-  //  for (x = 0; x < windowWidth; x = x + 70) {
-  //    for (y = 0; y < windowHeight; y = y + 70) {
-  //
-  //       fill(255, 105, 180, 63);
-  //       heart(x, y, 60, 60);
-  //       fill(248, 200, 220, 63);
-  //       heart(x,y,50, 40);
-  //       fill(255, 182, 193, 63);
-  //       heart(x, y, 40, 30);
-  //       size = size - 0.5;}
-  //
-  //   fill(0);
-  //   textSize(50);
-  //   textAlign(CENTER);
-  //   text('MADELINE', windowWidth/2, windowHeight/2);
-  //   pop();
-   
+  fill(255);
+  noStroke();
+  rect(600, 300, 240, 60);
+
+  fill(0);
+  textSize(50);
+  textAlign(CENTER);
+  text('MADELINE', windowWidth/2, windowHeight/2);
+  pop();
+
+  fill(255);
+  noStroke();
+  rect(295, 330, 100, 15);
+  let a = createA('https://editor.p5js.org/mwadsworth/full/J7T2ddODc', 'Self-Portrait');
+  a.position(300, 330);
+
+  rect(995, 330, 100, 15);
+  let m = createA('https://editor.p5js.org/mwadsworth/full/y3kH-dv8h', 'Abstract Art');
+  m.position(1000,330);
+
+  rect(625, 230, 160, 15);
+  let k = createA('https://editor.p5js.org/mwadsworth/full/p8O0S9r3t', 'Microsoft Paint Project');
+  k.position(630, 230);
+
+  rect(640, 430, 130, 15);
+  let l = createA('https://editor.p5js.org/mwadsworth/full/KTXoI0JyP', 'Animation Project');
+  l.position(645,430);
 }
+
 
 function heart(x, y, size) {
     beginShape();
